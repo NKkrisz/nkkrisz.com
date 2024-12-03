ce26f08424a7098a699b556dc775bc0183dc3f5a
NKkrisz <nkkrisz2005@gmail.com>
2024-12-03 14:04:37 +0100

    Init

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+      <h1>NKkrisz</h1>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
