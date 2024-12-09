db914529ed068f7829a05ff7d331ef1db12e3f75
NKkrisz <nkkrisz2005@gmail.com>
2024-12-09 23:55:31 +0100

    GitHub pages and temporary site

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ function App() {
   return (
     <>
     <NavBar/>
+    <div className="page" style={{marginTop:"0.5rem", padding:"0.5rem", color:"white"}}>
+      <h2>Work In Progress</h2>
+      <p>In The Meantime, Please Go To These Places:</p>
+      <a href="https://links.nkkrisz.com">My Socials / Contact Me</a>
+      <a href="https://kofi.nkkrisz.com">Donate To Me On Kofi</a>
+    </div>
     </>
   )
 }
